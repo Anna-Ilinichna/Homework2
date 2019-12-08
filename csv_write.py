7cdaef5049be8cb6fbf41f9266d5090e8396802a
Anna-Ilinichna <57904879+Anna-Ilinichna@users.noreply.github.com>
2019-12-08 16:39:40 +0300

    Update csv_write.py

--- csv_write.py
+++ csv_write.py
@@ -10,16 +10,3 @@ with open('homework.csv', 'w', newline='') as f:
     writer = csv.DictWriter(f, fieldnames=users[0], delimiter=';')   
     writer.writeheader()
     writer.writerow(users)
-
-
-#      import csv
-
-# people = [
-# #     {"name": "Tom", "age": 10, "city": "Moscow"},
-# #     {"name": "Mark", "age": 5, "city": "Khabarovsk"},
-# #     {"name": "Pam", "age": 7, "city": "Sahalin"}
-# ]
-# with open('people.csv', 'w', newline='') as file:
-#     writer = csv.DictWriter(file, fieldnames=people[0])
-#     writer.writeheader()
-#     writer.writerows(people)
